3bc497cd170c893be44f4c4194b2b8d136e688b6
William Antoline <65105895+williamantoline@users.noreply.github.com>
2023-07-16 22:56:52 +0700

    [WIP] appbar

--- lib/screens/base_screen.dart
+++ lib/screens/base_screen.dart
@@ -2,10 +2,10 @@ import 'package:bp/providers/theme_mode_provider.dart';
 import 'package:bp/screens/dashboard_screen.dart';
 import 'package:bp/screens/discover_screen.dart';
 import 'package:bp/screens/home_screen.dart';
+import 'package:bp/screens/profile_screen.dart';
 import 'package:bp/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 
 class BasePage extends StatefulWidget {
   const BasePage({super.key});
@@ -39,18 +39,63 @@ class _BasePageState extends State<BasePage> {
   @override
   Widget build(BuildContext context) {
     final setting = Provider.of<ThemeModeProvider>(context);
-    final currentWidth = MediaQuery.of(context).size.width;
+    // final currentWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       backgroundColor: setting.backgroundColor,
       appBar: AppBar(
-        // title: Text(_widgetOptions[_selectedIndex][0]),
         title: Text(_pageNames[_selectedIndex]),
         centerTitle: true,
         backgroundColor: $primary500,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.push(
+              context, 
+              MaterialPageRoute(
+                builder: (context) => const ProfilePage()
+              )
+            );
+          }, 
+          icon: const Icon(Icons.account_circle),
+        ),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  value: "add",
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(children: const [
+                      Icon(
+                        Icons.person_add_alt_1,
+                        color: Colors.black,
+                      ),
+                      Padding(padding: EdgeInsets.only(left: 10)),
+                      Text('New Contact'),
+                    ]),
+                  ),
+                )
+              ];
+            },
+            onSelected: (value) {
+              if (value == "add") {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const DiscoverPage(),
+                  ),
+                );
+              }
+            },
+          )
+        ],
       ),
       body: SingleChildScrollView(
-        // child: _widgetOptions[_selectedIndex][1],
         child: _widgetOptions[_selectedIndex],
       ),
       bottomNavigationBar: BottomNavigationBar(
